73c55bca79fe2b388764347fd909a13af4000ed6
Angela Gadon <apgadon@gmail.com>
2020-04-22 00:20:31 -0700

    Implemented js connection to server

--- chat_functionality.js
+++ chat_functionality.js
@@ -1,5 +1,16 @@
 /*This function ensures all of the HTML has loaded before any javascript kicks in.*/
 window.onload = function () {
-    
+	/*jQuery ajax GET request*/
+	$.ajax({
+		url:'https://chatbot-server4800.herokuapp.com/',
+		headers: {"Content-Type": "application/json"},
+		type:"GET",
+		success: function(result){
+			console.log(result)
+		},
+		error: function(error){
+			console.log('Error ${error}')
+		}
+	})
 };
 
