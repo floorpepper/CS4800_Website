ac6a7a33bd64a99637cf441194d5a4d47086ae17
Angela Gadon <apgadon@gmail.com>
2020-05-02 05:02:42 -0700

    Implementing chat functionality and adding logout button

--- chat_functionality.js
+++ chat_functionality.js
@@ -1,10 +1,77 @@
+/*
+for testing: 
+	username: user0 
+	password: pass0
+user0 token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ZWFiZTUyNTNlNmJkMjAwMTc4ODg5YTEiLCJpYXQiOjE1ODg0MDY2MTh9.Joorfc8KgJDbe06k3l5V-3l-vwh4uCmDyldWDKX28mU"
+*/
+
+var timeoutDefault = 700;
+
 /*This function ensures all of the HTML has loaded before any javascript kicks in.*/
 window.onload = function () {
-	
-	document.getElementById("loginBtn").addEventListener("click", loginReq);
-	document.getElementById("signupBtn").addEventListener("click", signupReq);
-	
+
+	assignFunction("loginBtn", loginReq);
+	assignFunction("signupBtn", signupReq);
+	assignFunction("logoutBtn", logout);
+	assignFunction("submitBtn", submit);
 };
+
+/*add event listeners to elements in html */
+function assignFunction(desiredElemId, desiredFunction){
+	var desiredElem = document.getElementById(desiredElemId);
+    if(desiredElem){
+        desiredElem.addEventListener("click", desiredFunction);
+    }
+}
+
+/*
+TODO: fix this function. Currently able to send a hard-coded message, but throws a CORS error: "Access to XMLHttpRequest at 'http://chatbot-server4800.herokuapp.com/messages' from origin 'null' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource."
+when a dynamic variable is used (aka userText, a variable that extracts user input from the webpage), a post request only works 
+part of the time. otherwise, gives CORS error.
+- look into CORS
+- see if this is a server issue
+*/
+/*send user response to server*/
+function submit() {
+	//add to chat visually
+	var userText = document.getElementById("userchat").value;
+	addToChat(true, userText);
+	var tester = "feeling bad";
+	var settings = {
+	  "url": "http://chatbot-server4800.herokuapp.com/messages",
+	  "method": "POST",
+	  "timeout": 0,
+	  "headers": {
+	    "Content-Type": "application/json",
+	    "Authorization": `Bearer ${"" + localStorage.token}`
+	  },
+	  "data": `{ \r\n\t\"message\": \"${userText}\"\r\n}`	//<-- problem area. Directly referencing userText in this string causes CORS error (sometimes works though)
+	};
+
+	$.ajax(settings).done(function (response) {
+	  console.log(response);
+	  var therapistResponse = response.newMessage;
+	  addToChat(false, therapistResponse);
+	});
+}
+
+function addToChat(isUser, chatText){
+	var node = document.createElement("LI");
+	
+	if(isUser){
+		node.className = "inlineRight";
+		node.classList.add("response");
+	}
+	else{
+		node.className = "inlineLeft";
+		node.classList.add("chat");
+	}
+
+	var textnode = document.createTextNode(chatText);
+	node.appendChild(textnode);
+	document.getElementById("chatList").appendChild(node);
+	document.getElementById("userchat").value = "";	//reset user's text box
+}
 
 /*Log in as an existing user*/
 function loginReq(){
@@ -32,12 +99,14 @@ function loginReq(){
 	$.ajax(settings).done(function (response) {
 		
 		document.getElementById("loginResult").innerHTML = response.message;
-		
+		localStorage.token = JSON.stringify(response.user.token).slice(1, -1);
+		alert("token now " + localStorage.token);
 	  	console.log(response);
+	  	window.setTimeout(function(){
+	 		//relocate to chatroom
+			window.location.href = "chatroom.html";
+	 	}, timeoutDefault);
 	});
-	
-	//relocate to chatroom
-	//window.location.href = "chatroom.html";
 }
 
 /*Create a new user*/
@@ -62,14 +131,19 @@ function signupReq(){
 	$.ajax(settings).done(function (response) {
 
 		document.getElementById("signupResult").innerHTML = response.message;//JSON.stringify(response);
-	  console.log(response);
+		localStorage.token = JSON.stringify(response.user.token).slice(1, -1);
+	 	console.log(response);
+	 	window.setTimeout(function(){
+	 		//relocate to chatroom
+			window.location.href = "chatroom.html";
+	 	}, timeoutDefault);
 	});
-	
-	//relocate to chatroom
-	//window.location.href = "chatroom.html";
 }
 
-
+function logout(){
+	localStorage.token = "-1";	//reset token
+	window.location.href = "homepage.html";
+}
 
 function putReq(){
 	/*jQuery ajax POST request*/
